aac3bfdfc608b60b7f41079012e16407dc271eb1
Eric Chabert <Eric.Chabert@cern.ch>
2020-10-07 11:30:43 +0200

    correct bug for bool reading

--- python/Loader.py
+++ python/Loader.py
@@ -17,15 +17,17 @@ def LoadCand(line):
     while len(words)>0:
         cluster = Cluster()
         cluster.layer = int(words.pop(0))
-        cluster.path = float(words.pop(0))
+        cluster.pathlength = float(words.pop(0))
         cluster.eloss = float(words.pop(0))
-        cluster.shape = bool(words.pop(0))
-        cluster.edge = bool(words.pop(0))
-        cluster.cut = bool(words.pop(0))
-        cluster.clean =bool(words.pop(0))
+        #important remark: conversion string --> int --> bool. Cannot convert directly string to bool otherwise it is always true !
+	cluster.shape = bool(int(words.pop(0)))
+        cluster.edge = bool(int(words.pop(0)))
+        cluster.cut = bool(int(words.pop(0)))
+        cluster.clean =bool(int(words.pop(0)))
         cluster.nsat = int(words.pop(0))
         cluster.nstrips = int(words.pop(0))
-        cluster.detid = int(words.pop(0))
+        cluster.xtalkVar = float(words.pop(0))
+	cluster.detid = int(words.pop(0))
         clusters.append(cluster)
     cand = HSCPCand(p,pt,eta,clusters)
     # return both event and cand
